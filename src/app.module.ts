4d8284b707150e7f7b2efbcc3476bce3e5d94650
Gael Borchardt <gaelborchardt@gmail.com>
2025-03-03 21:55:02 -0700

    added folders

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,6 +9,18 @@ import { UsersController } from './users/users.controller';
 import 'dotenv/config';
 import { UsersService } from './users/users.service';
 import { TicketsModule } from './tickets/tickets.module';
+import { OrdersService } from './orders/orders.service';
+import { OrdersController } from './orders/orders.controller';
+import { OrdersModule } from './orders/orders.module';
+import { MenuItemsService } from './menu_items/menu_items.service';
+import { MenuItemsController } from './menu_items/menu_items.controller';
+import { MenuItemsModule } from './menu_items/menu_items.module';
+import { RolesService } from './roles/roles.service';
+import { RolesController } from './roles/roles.controller';
+import { RolesModule } from './roles/roles.module';
+import { OrderToppingsService } from './order_toppings/order_toppings.service';
+import { OrderToppingsController } from './order_toppings/order_toppings.controller';
+import { OrderToppingsModule } from './order_toppings/order_toppings.module';
 
 @Module({
   imports: [
@@ -25,9 +37,13 @@ import { TicketsModule } from './tickets/tickets.module';
       autoLoadEntities: true,
     }),
     TicketsModule,
+    OrdersModule,
+    MenuItemsModule,
+    RolesModule,
+    OrderToppingsModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [AppController, OrdersController, MenuItemsController, RolesController, OrderToppingsController],
+  providers: [AppService, OrdersService, MenuItemsService, RolesService, OrderToppingsService],
 })
 export class AppModule {
   constructor(private dataSource: DataSource) {}
