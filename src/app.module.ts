ec15c9c4a2e655bffb4f073fa2bbab5c262694c4
Naraka28 <eildan28@hotmail.com>
2025-03-10 17:57:16 -0700

    borre modulos del app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,16 +8,12 @@ import { UsersModule } from './users/users.module';
 import { User } from './users/entity/user.entity';
 import { TicketsModule } from './tickets/tickets.module';
 import { OrdersModule } from './orders/orders.module';
-import { MenuItemsModule } from './menu_items/menu_items.module';
 import { RolesModule } from './roles/roles.module';
 import { OrderToppingsModule } from './order_toppings/order_toppings.module';
 import { SizesModule } from './sizes/sizes.module';
 import { MilksModule } from './milks/milks.module';
-import { ItemFlavoursModule } from './item_flavours/item_flavours.module';
 import { ToppingsModule } from './toppings/toppings.module';
 import { FlavoursModule } from './flavours/flavours.module';
-import { InventoryModule } from './inventory/inventory.module';
-import { ItemToppingsModule } from './item_toppings/item_toppings.module';
 import { Roles } from './roles/entity/roles.entity';
 
 @Module({
@@ -36,16 +32,12 @@ import { Roles } from './roles/entity/roles.entity';
     }),
     TicketsModule,
     OrdersModule,
-    MenuItemsModule,
     RolesModule,
     OrderToppingsModule,
     SizesModule,
     MilksModule,
-    ItemFlavoursModule,
     ToppingsModule,
     FlavoursModule,
-    InventoryModule,
-    ItemToppingsModule,
   ],
   controllers: [AppController],
   providers: [AppService],
