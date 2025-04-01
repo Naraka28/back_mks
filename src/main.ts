b38634cf3a68aafe8d6bd4b77a41ceb9b6701fd1
Naraka28 <eildan28@hotmail.com>
2025-04-01 11:31:29 -0700

    swagger set up, still in progress

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.setGlobalPrefix("mks/");
+  app.setGlobalPrefix('mks/');
+
+  const config = new DocumentBuilder()
+    .setTitle('MokkaSoft Endpoints')
+    .setDescription('MokkaSoft API Endpoints')
+    .setVersion('1.0')
+    .addTag('mks')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('swagger', app, documentFactory);
 
   await app.listen(process.env.PORT ?? 3000);
 }
--- src/orders/dto/create-orders.dto.ts
+++ src/orders/dto/create-orders.dto.ts
@@ -1,27 +1,36 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { Type } from 'class-transformer';
 import { IsArray, IsInt, ValidateNested } from 'class-validator';
 import { ToppingOrderDto } from 'src/toppings/dto/create-toppings-orders.dto';
 
 export class CreateOrderDto {
+  @ApiProperty()
   @IsInt()
   productId: number;
 
+  @ApiProperty()
   @IsInt()
   flavoursId: number;
 
+  @ApiProperty()
   @IsInt()
   sizesId: number;
 
+  @ApiProperty()
   @IsInt()
   tempId: number;
 
+  @ApiProperty()
   @IsArray()
   @ValidateNested({ each: true })
   @Type(() => ToppingOrderDto)
   toppings: ToppingOrderDto[];
+
+  @ApiProperty()
   @IsInt()
   milksId: number;
 
+  @ApiProperty()
   @IsInt()
   cashierId: number;
 }
