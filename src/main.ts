1ddddf6451342ffcd24e001e339f0632ff580816
Gael Borchardt <117969020+dsGael@users.noreply.github.com>
2025-04-03 23:48:43 -0700

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,13 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+
+  app.enableCors({
+    origin: ['http://localhost:5123'], // your Vite frontend dev server
+   
+  });
+  
   app.setGlobalPrefix('mks/');
 
   const config = new DocumentBuilder()
