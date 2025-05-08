00f5c072c41bba8189f552096f784715686793af
Gael Borchardt <gaelborchardt@gmail.com>
2025-05-07 17:33:13 -0700

    created orders toppings | changes in create orders are pending

--- src/order_toppings/order_toppings.service.ts
+++ src/order_toppings/order_toppings.service.ts
@@ -1,4 +1,39 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { OrderToppings } from './entity/order_toppings.entity';
 
 @Injectable()
-export class OrderToppingsService {}
+export class OrderToppingsService {
+  constructor(
+    @InjectRepository(OrderToppings)
+    private readonly orderToppingsRepository: Repository<OrderToppings>,
+  ) {}
+
+
+    async findAll(): Promise<OrderToppings[]> {
+        return this.orderToppingsRepository.find({ relations: ['topping'] });
+    }
+
+    async findOne(id: number): Promise<OrderToppings | null> {
+        return this.orderToppingsRepository.findOne({ where: { id }, relations: ['topping'] });
+
+    }
+
+    async findByOrderId(orderId: number): Promise<OrderToppings[]> {
+        return this.orderToppingsRepository.find({ where: { order: { id: orderId } }, relations: ['topping'] });
+    }
+
+    async create(orderTopping: Partial<OrderToppings>): Promise<OrderToppings> {
+        return this.orderToppingsRepository.save(orderTopping);
+    }
+
+    // async update(id: number, orderTopping: Partial<OrderToppings>): Promise<OrderToppings> {
+    //     await this.orderToppingsRepository.update(id, orderTopping);
+    //     return this.findOne(id);
+    // }
+    // async remove(id: number): Promise<void> {
+    //     await this.orderToppingsRepository.delete(id);
+    // }
+
+}
--- src/orders/orders.module.ts
+++ src/orders/orders.module.ts
@@ -4,10 +4,15 @@ import { Orders } from './entity/orders.entity';
 import { OrdersService } from './orders.service';
 import { OrdersController } from './orders.controller';
 import { User } from 'src/users/entity/user.entity';
-import { Flavours } from 'src/flavours/entity/flavours.entity';
+import { OrderToppingsModule } from 'src/order_toppings/order_toppings.module';
+import { OrderToppings } from 'src/order_toppings/entity/order_toppings.entity';
+
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Orders, User])],
+  imports: [
+    TypeOrmModule.forFeature([Orders, User, OrderToppings]),
+    OrderToppingsModule,
+  ],
   providers: [OrdersService],
   controllers: [OrdersController],
 })
